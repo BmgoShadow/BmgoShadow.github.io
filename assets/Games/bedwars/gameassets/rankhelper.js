3534b39b13268ccc389ae16b276d0807e95dcf2a
d17031 <teammicro2026@outlook.com>
2026-01-17 10:41:43 +0000

    Update rankhelper.js

--- assets/Games/bedwars/gameassets/rankhelper.js
+++ assets/Games/bedwars/gameassets/rankhelper.js
@@ -1,1 +1,7 @@
+// game.js
+import { database } from "./database.js";
 
+function onGameWin(userId) {
+    const newRank = database.addStar(userId);
+    console.log(`⭐ User ${userId} now has ${newRank} Stars!`);
+}
